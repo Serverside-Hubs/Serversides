1b405cb7f900d7d609b703565d73b2ea3cf85f09
Serverside-Hubs <133831048+Serverside-Hubs@users.noreply.github.com>
2023-12-08 16:06:12 -0800

    Update Azure.lua

--- Azure.lua
+++ Azure.lua
@@ -1,5 +1,17 @@
+local _ENV = table.create(0);
+--[[ String | any ]]
 local Table = table.create(0);
+--[[ String | number ]]
 Table["Standard"] = 144853052;
 Table["Premium"] = 145191525;
 Table["Premiun+"] = 0;
-return Table
+_ENV.Passes = Table
+local Table = table.create(0);
+--[[ UserId | boolean ]]
+Table[1234567890] = true
+_ENV.Blacklisted = Table
+local Table = table.create(0);
+--[[ UserId | boolean ]]
+Table[1234567890] = true
+_ENV.Whitelisted = Table
+return _ENV
